99c80c0678ecf48aa8284db02e7ffcab9d463bb0
Clodus-NT <clodus.nt@gmail.com>
2022-05-18 14:18:33 -0400

    task two is now complete

--- src/components/task-two/task-two.js
+++ src/components/task-two/task-two.js
@@ -6,9 +6,21 @@ export default function TaskTwo() {
   const [btnRedId, setBtnRedId] = useState('btn-red');
   const [btnYellowId, setBtnYellowId] = useState('btn-yellow');
 
-  const swapRed = () => {
-    if (btnRedId === 'btn-red') {
-      setBtnRedId((btnRedId) => btnRedId === 'btn-yellow')
+  const swapColor = () => {
+    let nextRedId;
+    let nextYellowId;
+    if (btnRedId === 'btn-red' && btnYellowId === 'btn-yellow') {
+      nextRedId = 'btn-yellow'
+      nextYellowId = 'btn-red'
+
+      setBtnRedId(nextRedId)
+      setBtnYellowId(nextYellowId)
+    } else if (btnRedId === 'btn-yellow' && btnYellowId === 'btn-red') {
+      nextRedId = 'btn-red'
+      nextYellowId = 'btn-yellow'
+
+      setBtnRedId(nextRedId)
+      setBtnYellowId(nextYellowId)
     }
   }
 
@@ -18,11 +30,12 @@ export default function TaskTwo() {
     <div id="two-btn-div">
       <button 
         id={btnRedId.toString()}
-        onClick={() => swapRed()}
+        onClick={() => swapColor()}
       >
       </button>
       <button 
         id={btnYellowId.toString()}
+        onClick={() => swapColor()}
       ></button>
     </div>
     </div>
