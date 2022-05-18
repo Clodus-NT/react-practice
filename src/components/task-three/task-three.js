8456867736d5e5897c78572c31f8e2320e857b6b
Clodus-NT <clodus.nt@gmail.com>
2022-05-18 15:54:43 -0400

    created and styled elements for task three

--- src/components/task-three/task-three.js
+++ src/components/task-three/task-three.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './task-three.css';
+
 
 export default function TaskThree() {
+  const [tile, setTile] = useState('');
+
   return(
-    <div>Task Three</div>
+    <>
+      <div id='div-three'>
+        <button id='dlt-btn-one'>One</button>
+        <button id='dlt-btn-two'>Two</button>
+        <button id='dlt-btn-three'>Three</button>
+        <button id='dlt-btn-four'>Four</button>
+      </div>
+      <div id='reset-btn-three'>
+        <button>Reset</button>
+      </div>
+    </>
   )
 }
